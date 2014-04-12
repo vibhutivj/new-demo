81727d244282e515b5813e2a7c5e957f4b95ec84
Vibhuti <vibhutivj@hotmail.com>
2014-04-12 13:19:24 +0530

    new

---
+++ apps/Demo/android/native/assets/www/default/js/views/HomeView.js
@@ -0,0 +1,22 @@
+
+/* JavaScript content from js/views/HomeView.js in folder common */
+app.Views.HomeView=(function(){
+	var that=this;
+	this.render=function(data)
+	{
+		$("#mainpage").html(data);
+		$("#mainpage").trigger('pagecreate');
+	};
+	this.initialize=function()
+	{
+		alert("home view initialize");
+		console.log(this);
+		$.get("templates/HomeView.html",that.render,'html');
+	};
+	var __construct=function()
+	{
+		alert("homneview instance");
+		that.initialize();
+	}();
+	
+});
---
+++ apps/Demo/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":3882831116,"date":1395143884880,"machine":"COM4"};
+/* Date: Tue Mar 18 17:28:04 IST 2014 */
